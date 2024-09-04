2817556df38ad92129d2d00c04c9e0c679251359
Draginalo <tropicaltisa@gmail.com>
2024-09-04 19:19:38 -0400

    Added bounding walls.

--- examples/flocking/behaviours/BoundedAreaRule.cpp
+++ examples/flocking/behaviours/BoundedAreaRule.cpp
@@ -2,6 +2,7 @@
 #include "../gameobjects/Boid.h"
 #include "../gameobjects/World.h"
 #include "engine/Engine.h"
+#include <iostream>
 
 Vector2f BoundedAreaRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   // Return a force proportional to the proximity of the boids with the bounds, and opposed to it
@@ -9,6 +10,36 @@ Vector2f BoundedAreaRule::computeForce(const std::vector<Boid*>& neighborhood, B
 
   // todo: add here your code code here do make the boid follow the bounded box rule
   // hint: use this->world->engine->window->size() and desiredDistance
+
+  //Could use dot product, but using position is less process intensive here
+
+  Vector2f lookAheadTarget = boid->getPosition() + boid->velocity.normalized();
+  Point2D windowSize = this->world->engine->window->size();
+
+  if (lookAheadTarget.y > windowSize.y - desiredDistance) { //Bottom wall
+    float distenceToReflect = abs(boid->getPosition().y - windowSize.y);
+    Vector2f wallNormal = {0, -1};
+
+    force += wallNormal * distenceToReflect;
+  }
+  if (lookAheadTarget.y < 0 + desiredDistance) { //Top wall
+    float distenceToReflect = abs(0 - boid->getPosition().y);
+    Vector2f wallNormal = {0, 1};
+
+    force += wallNormal * distenceToReflect;
+  }
+  if (lookAheadTarget.x > windowSize.x - desiredDistance) { //Right wall
+    float distenceToReflect = abs(boid->getPosition().x - windowSize.x);
+    Vector2f wallNormal = {-1, 0};
+
+    force += wallNormal * distenceToReflect;
+  }
+  if (lookAheadTarget.x < 0 + desiredDistance) { //Right wall
+    float distenceToReflect = abs(0 - boid->getPosition().x);
+    Vector2f wallNormal = {1, 0};
+
+    force += wallNormal * desiredDistance;
+  }
 
   return force;
 }
