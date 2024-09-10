99f77ce0b294dd3ab7af33ace3005fcdb3c51075
Draginalo <tropicaltisa@gmail.com>
2024-09-10 18:22:19 -0400

    Did hexegonal game of life.

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -3,5 +3,60 @@
 //
 
 #include "HexagonGameOfLife.h"
-void HexagonGameOfLife::Step(World& world) {}
-int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { return 0; }
+#include <iostream>
+
+void HexagonGameOfLife::Step(World& world) {
+  for (int i = 0; i < world.SideSize(); i++) {
+    for (int j = 0; j < world.SideSize(); j++) {
+      const bool currSpace = world.Get({i, j});
+      const int liveNeighborCount = CountNeighbors(world, {i, j});
+      if (currSpace) {
+        if (liveNeighborCount < 2) {
+          world.SetNext({i, j}, false);
+        }
+        else if (liveNeighborCount <= 3) {
+          world.SetNext({i, j}, true);
+        }
+        else {
+          world.SetNext({i, j}, false);
+        }
+      }
+      else {
+        if (liveNeighborCount == 2) {
+          world.SetNext({i, j}, true);
+        }
+      }
+    }
+  }
+}
+
+int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
+  int start = point.x - static_cast<int>(point.y % 2 == 0);
+  int count = 0;
+
+  //Calculates live neighbors above
+  for (int i = start; i < start + 2; i++) {
+    if (world.Get({i, point.y - 1})) {
+      count++;
+    }
+  }
+
+  //Calculates live neighbors below
+  for (int i = start; i < start + 2; i++) {
+    if (world.Get({i, point.y + 1})) {
+      count++;
+    }
+  }
+
+  //Calculates live neighbors on sides
+  start = point.x - 1;
+  for (int i = start; i < start + 3; i++) {
+    if (i != point.x) {
+      if (world.Get({i, point.y})) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+ }
