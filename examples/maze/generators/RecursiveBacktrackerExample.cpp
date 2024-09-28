026bc0d46e67c87005b869b51b92799beed27faf
Draginalo <tropicaltisa@gmail.com>
2024-09-28 11:59:38 -0400

    Made recursive DFS maze.

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -1,6 +1,8 @@
 #include "../World.h"
 #include "Random.h"
 #include "RecursiveBacktrackerExample.h"
+
+#include <array>
 #include <climits>
 bool RecursiveBacktrackerExample::Step(World* w) {
   if (stack.empty() && visitedIsEmpty()) {
@@ -8,57 +10,53 @@ bool RecursiveBacktrackerExample::Step(World* w) {
   }
 
  if (!stack.empty()) {
-    Point2D point = stack.back();
-    Node node = w->GetNode(point);
-    std::vector<Point2D> neighbors = getVisitables(w, point);
+   Point2D const point = stack.back();
+   Node node = w->GetNode(point);
+   std::vector<Point2D> neighbors = getVisitables(w, point);
 
- if (neighbors.size() > 0) {
-   w->SetNodeColor(point, Color::Blue);
-   visited[point.y][point.x] = 1;
+   if (!neighbors.empty()) {
+    w->SetNodeColor(point, Color::Blue);
+    visited[point.y][point.x] = true;
 
-   Point2D nextPoint = neighbors[getRandomNumber()%neighbors.size()];
-   stack.push_back(nextPoint);
+    Point2D const nextPoint = neighbors[getRandomNumber()%neighbors.size()];
+    stack.push_back(nextPoint);
 
-   Point2D direction = nextPoint - point;
+    Point2D const direction = nextPoint - point;
 
-   if (direction.y == -1) {
-    node.SetNorth(0);
-   }
-   else if (direction.x == 1) {
-     node.SetEast(0);
-   }
-   else if (direction.y == 1) {
-     node.SetSouth(0);
-   }
-   else if (direction.x == -1) {
-     node.SetWest(0);
-   }
+    if (direction.y == -1) {
+     node.SetNorth(false);
+    }
+    else if (direction.x == 1) {
+      node.SetEast(false);
+    }
+    else if (direction.y == 1) {
+      node.SetSouth(false);
+    }
+    else if (direction.x == -1) {
+      node.SetWest(false);
+    }
 
-
-    w->SetNode(point, node);
-  }
-  else {
+     w->SetNode(point, node);
+  } else {
       Point2D point = stack.back();
       w->SetNodeColor(point, Color::Green);
       stack.pop_back();
     }
   }
 
-  return true;
+  return !stack.empty();
 }
 
 int RecursiveBacktrackerExample::getRandomNumber() {
   static int randomIndex = 0;
-  int const randomInts[]
-      = {72, 99, 56, 34, 43, 62, 31, 4,  70, 22, 6,  65, 96, 71, 29, 9,  98, 41, 90, 7,  30, 3,  97, 49, 63, 88, 47, 82, 91, 54, 74, 2,  86, 14,
-         58, 35, 89, 11, 10, 60, 28, 21, 52, 50, 55, 69, 76, 94, 23, 66, 15, 57, 44, 18, 67, 5,  24, 33, 77, 53, 51, 59, 20, 42, 80, 61, 1,  0,
-         38, 64, 45, 92, 46, 79, 93, 95, 37, 40, 83, 13, 12, 78, 75, 73, 84, 81, 8,  32, 27, 19, 87, 85, 16, 25, 17, 68, 26, 39, 48, 36};
+  const std::array randomInts = {72, 99, 56, 34, 43, 62, 31, 4, 70, 22, 6, 65, 96, 71, 29, 9, 98, 41, 90, 7, 30, 3, 97, 49, 63, 88, 47, 82, 91, 54, 74, 2, 86, 14, 58, 35, 89, 11, 10, 60, 28, 21, 52, 50, 55, 69, 76, 94, 23, 66, 15, 57, 44, 18, 67, 5, 24, 33, 77, 53, 51, 59, 20, 42, 80, 61, 1, 0, 38, 64, 45, 92, 46, 79, 93, 95, 37, 40, 83, 13, 12, 78, 75, 73, 84, 81, 8, 32, 27, 19, 87, 85, 16, 25, 17, 68, 26, 39, 48, 36};
 
   if (randomIndex == std::size(randomInts)) {
     randomIndex = 0;
   }
   return randomInts[randomIndex++];
 }
+
 bool RecursiveBacktrackerExample::visitedIsEmpty() {
   int count = 0;
   for (auto it : visited)  {
@@ -96,16 +94,16 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   auto sideOver2 = w->GetSize() / 2;
   std::vector<Point2D> visitables;
 
-  if (p.y + 1 <= sideOver2 && !visited[p.y + 1][p.x] && w->GetNodeColor(p.Down()) != Color::Green) {
-    visitables.emplace_back(p.x, p.y + 1);
+  if (p.y - 1 >= -sideOver2 && !visited[p.y - 1][p.x] && w->GetNodeColor(p.Up()) != Color::Green) {
+    visitables.emplace_back(p.x, p.y - 1);
   }
 
   if (p.x + 1 <= sideOver2 && !visited[p.y][p.x + 1] && w->GetNodeColor(p.Right()) != Color::Green) {
     visitables.emplace_back(p.x + 1, p.y);
   }
 
-  if (p.y - 1 >= -sideOver2 && !visited[p.y - 1][p.x] && w->GetNodeColor(p.Up()) != Color::Green) {
-    visitables.emplace_back(p.x, p.y - 1);
+  if (p.y + 1 <= sideOver2 && !visited[p.y + 1][p.x] && w->GetNodeColor(p.Down()) != Color::Green) {
+    visitables.emplace_back(p.x, p.y + 1);
   }
 
   if (p.x - 1 >= -sideOver2 && !visited[p.y][p.x - 1] && w->GetNodeColor(p.Left()) != Color::Green) {
